c960a15814ff92f6dcf763cd95fddf52d9f4c892
mlkomou <malifoli.musique@gmail.com>
2024-03-27 13:04:19 +0000

    translate forms

--- src/app/cow/add-cow/add-cow.module.ts
+++ src/app/cow/add-cow/add-cow.module.ts
@@ -8,16 +8,28 @@ import { AddCowPageRoutingModule } from './add-cow-routing.module';
 
 import { AddCowPage } from './add-cow.page';
 import { ImageCropperModule } from 'ngx-image-cropper';
-
+import {TranslateLoader, TranslateModule} from "@ngx-translate/core";
+import {HttpClient} from "@angular/common/http";
+import {TranslateHttpLoader} from "@ngx-translate/http-loader";
+export function createTranslateLoader(http: HttpClient): any {
+  return new TranslateHttpLoader(http, "assets/i18n/", ".json");
+}
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    AddCowPageRoutingModule,
-    ReactiveFormsModule,
-    ImageCropperModule
-  ],
+    imports: [
+        CommonModule,
+        FormsModule,
+        IonicModule,
+        AddCowPageRoutingModule,
+        ReactiveFormsModule,
+        ImageCropperModule,
+      TranslateModule.forRoot({
+        loader: {
+          provide: TranslateLoader,
+          useFactory: createTranslateLoader,
+          deps: [HttpClient],
+        },
+      }),
+    ],
   declarations: [AddCowPage]
 })
 export class AddCowPageModule {}
--- src/app/farm/add-farm/add-farm.module.ts
+++ src/app/farm/add-farm/add-farm.module.ts
@@ -8,17 +8,19 @@ import { AddFarmPageRoutingModule } from './add-farm-routing.module';
 
 import { AddFarmPage } from './add-farm.page';
 import {ImageCropperModule} from "ngx-image-cropper";
+import {TranslateModule} from "@ngx-translate/core";
 
 @NgModule({
-  imports: [
-    ReactiveFormsModule,
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    AddFarmPageRoutingModule,
-    ReactiveFormsModule,
-    ImageCropperModule
-  ],
+    imports: [
+        ReactiveFormsModule,
+        CommonModule,
+        FormsModule,
+        IonicModule,
+        AddFarmPageRoutingModule,
+        ReactiveFormsModule,
+        ImageCropperModule,
+        TranslateModule
+    ],
   declarations: [AddFarmPage]
 })
 export class AddFarmPageModule {}
