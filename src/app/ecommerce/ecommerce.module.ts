754c4d0ea24c6eaea30391c85f2ea236de085b58
mlkomou <malifoli.musique@gmail.com>
2024-05-26 11:25:59 +0000

    start translation update

--- src/app/ecommerce/ecommerce.module.ts
+++ src/app/ecommerce/ecommerce.module.ts
@@ -7,14 +7,16 @@ import { IonicModule } from '@ionic/angular';
 import { EcommercePageRoutingModule } from './ecommerce-routing.module';
 
 import { EcommercePage } from './ecommerce.page';
+import {TranslateModule} from "@ngx-translate/core";
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    EcommercePageRoutingModule
-  ],
+    imports: [
+        CommonModule,
+        FormsModule,
+        IonicModule,
+        EcommercePageRoutingModule,
+        TranslateModule
+    ],
   declarations: [EcommercePage]
 })
 export class EcommercePageModule {}
